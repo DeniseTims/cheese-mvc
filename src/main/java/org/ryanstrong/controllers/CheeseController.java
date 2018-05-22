855ed89cd390cba9a874c4f278ce8899ad77f37e
Ryan Strong <ryanstrong2@yahoo.com>
2018-05-21 20:51:41 -0500

    setup cheeseDao

--- src/main/java/org/ryanstrong/controllers/CheeseController.java
+++ src/main/java/org/ryanstrong/controllers/CheeseController.java
@@ -3,11 +3,12 @@ package org.ryanstrong.controllers;
 //import models.Cheese;
 
 import org.ryanstrong.models.Cheese;
+import org.ryanstrong.models.data.CheeseDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
 
 // import org.mockMVC
 /**
@@ -17,13 +18,16 @@ import java.util.HashMap;
 @RequestMapping ("cheese")
 public class CheeseController {
 
-    static HashMap<String, String> cheeses = new HashMap<>();
+    @Autowired
+    private CheeseDao cheeseDao;
+
+//    static HashMap<String, String> cheeses = new HashMap<>();
 //    private List<Cheese> cheeses = new ArrayList<>();
    // ArrayList<String > cheeses = new ArrayList<>();
     //Request path: GET/cheese
     @RequestMapping(value = "")//, method = RequestMethod.GET)
     public String index(Model model) {
-        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("cheeses", cheeseDao.findAll());
         model.addAttribute("title", "My Cheeses");
         return "cheese/index";//my Cheese
     }
@@ -31,39 +35,46 @@ public class CheeseController {
     @RequestMapping(value ="add", method = RequestMethod.GET)
     public String displayAddCheeseForm(Model model){
         model.addAttribute("title", "Add Cheese" );
+        model.addAttribute(new Cheese());
         return "cheese/add";
         }
         //request path: Post /cheese/add
         @RequestMapping(value= "add", method = RequestMethod.POST)
-        public String processAddCheeseForm(
-//                @ModelAttribute Cheese newCheese)
-                @RequestParam String name, @RequestParam String description)
-        {
-            cheeses.put(name, description);
+        public String processAddCheeseForm(Model model,
+                @ModelAttribute Cheese newCheese, Errors errors)
+//                @RequestParam String name
+// , @RequestParam String description)
+        {if(errors.hasErrors()){
+            model.addAttribute("title", "New Report");
+            return "cheese/add/";
+        }
+//            cheeses.put(name, description);
 //            String cheeseList = Cheese.getName();
-//            cheeses.add(newCheese);
+//            cheeses. add(newCheese);
 //            cheeses.set(cheeses.getName, description);
+            cheeseDao.save(newCheese);
             // Redirect to /cheese
             return "redirect:";
         }
            // public String displayDeleteForm(Model model){
              //   model.addAttribute
     @RequestMapping(value="edit", method=RequestMethod.GET)
-    public String displayEditForm(Model model, @PathVariable int cheeseId)
+    public String displayEditForm(Model model, @PathVariable int id)
     {
-        model.addAttribute("cheeseName", cheeses);
+        model.addAttribute("cheeseName", cheeseDao.findAll());
         return "cheese/edit";
     }
     @RequestMapping(value="edit", method = RequestMethod.POST)
-    public String processEditForm(@PathVariable int cheeseId, String name,
+    public String processEditForm(@PathVariable int id, String name,
                                   String description){
                 return "cheese/edit";
     }
     @RequestMapping(value="remove", method=RequestMethod.GET)
-    public String displayRemoveUserForm(Model model
+    public String displayRemoveUserForm(Model model, @RequestParam int id
 //            , @RequestParam String cheeseName, @RequestParam String description
     ){
-        model.addAttribute("cheeseName",cheeses);
+//        Cheese cheese = cheeseDao.findAll();
+        model.addAttribute("cheeseName",cheeseDao.findAll());
 //        model.addAttribute("description", cheeses.get(description);
         return  "cheese/remove";
     }
--- src/main/java/org/ryanstrong/models/Cheese.java
+++ src/main/java/org/ryanstrong/models/Cheese.java
@@ -7,9 +7,10 @@ public class Cheese {
 //    @Id
 //    @GeneratedValue
     private int id;
+    private static int nextId =1;
 
     @NotNull
-    @Size(min=3, max=15)
+    @Size(min=1, max=15)
     private String name;
 
     @NotNull
@@ -18,14 +19,12 @@ public class Cheese {
 
     private Integer cheeseId;
 
-
     public Cheese(String name, String description){
         this();
         this.name=name;
         this.description=description;
     }
     public Cheese(){
-        Integer nextId =1;
         cheeseId = nextId;
         nextId++;
     }
@@ -44,4 +43,9 @@ public class Cheese {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(int id){this.id=id;}
 }
